dd11e9857eca5563d5e3204e2ef9090cc4c402d1
wgeorge1990 <williameverettgeorge@icloud.com>
2018-12-13 14:59:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+};
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+};
 
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedList.length; i < l; i++){ let children = rankedList[i].children
+
+  for (let a = 0, h = children.length; a < h; a++) {
+      children[a].innerHTML = parseInt(children[a].innerHTML) + n
+    }
+  }
+
+};
+
+function deepestChild(){
+    let node = document.getElementById('grand-node')
+    let childNode = node.children[0]
+  while (childNode){
+    node = childNode
+    childNode = node.children[0]
+  }
+  return node
+
+};
